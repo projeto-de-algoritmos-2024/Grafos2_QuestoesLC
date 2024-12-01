c91dcdb809b2c62dbbda4dbeec48f762feda5ad4
Leandro <leandroalmeidaoliveira650@gmail.com>
2024-12-01 15:57:45 -0300

    add contaggem pseudo

--- 1489. Find Critical and Pseudo-Critical Edges in Minimum Spanning Tree/1489. Find Critical and Pseudo-Critical Edges in Minimum Spanning Tree.py
+++ 1489. Find Critical and Pseudo-Critical Edges in Minimum Spanning Tree/1489. Find Critical and Pseudo-Critical Edges in Minimum Spanning Tree.py
@@ -1,8 +1,8 @@
 from typing import List
 import heapq
 
-class Solucao:
-    def encontrarArestasCriticas(self, n_nos: int, arestas: List[List[int]]) -> List[int]:
+class Solution:
+    def encontrarArestasCriticasEPseudoCriticas(self, n_nos: int, arestas: List[List[int]]) -> List[List[int]]:
         for indice, aresta in enumerate(arestas):
             aresta.append(indice)
         
@@ -11,16 +11,30 @@ class Solucao:
             grafo[inicio].append((peso, fim, indice))
             grafo[fim].append((peso, inicio, indice))
         
-        def prim(excluir_aresta: int = -1) -> int:
+        def prim(excluir_aresta: int = -1, incluir_aresta: int = -1) -> int:
             visitado = [False] * n_nos
             custo_mst = 0
             arestas_usadas = 0
             heap = []
             
-            visitado[0] = True
-            for peso, vizinho, indice_aresta in grafo[0]:
-                if indice_aresta != excluir_aresta:
-                    heapq.heappush(heap, (peso, vizinho, indice_aresta))
+            if incluir_aresta != -1:
+                inicio, fim, peso, _ = arestas[incluir_aresta]
+                custo_mst += peso
+                visitado[inicio] = True
+                visitado[fim] = True
+                arestas_usadas += 1
+                for prox_peso, vizinho, indice_aresta in grafo[inicio]:
+                    if indice_aresta != excluir_aresta:
+                        heapq.heappush(heap, (prox_peso, vizinho, indice_aresta))
+                for prox_peso, vizinho, indice_aresta in grafo[fim]:
+                    if indice_aresta != excluir_aresta:
+                        heapq.heappush(heap, (prox_peso, vizinho, indice_aresta))
+
+            if incluir_aresta == -1:
+                visitado[0] = True
+                for peso, vizinho, indice_aresta in grafo[0]:
+                    if indice_aresta != excluir_aresta:
+                        heapq.heappush(heap, (peso, vizinho, indice_aresta))
             
             while heap and arestas_usadas < n_nos - 1:
                 peso, no, indice_aresta = heapq.heappop(heap)
@@ -38,17 +52,20 @@ class Solucao:
         custo_mst_original = prim()
 
         arestas_criticas = []
+        arestas_pseudo_criticas = []
 
         for indice, (inicio, fim, peso, indice_original) in enumerate(arestas):
             if prim(excluir_aresta=indice_original) > custo_mst_original:
                 arestas_criticas.append(indice_original)
+            elif prim(incluir_aresta=indice_original) == custo_mst_original:
+                arestas_pseudo_criticas.append(indice_original)
         
-        return arestas_criticas
+        return [arestas_criticas, arestas_pseudo_criticas]
 
-# Exemplo de uso
-solucao = Solucao()
+solucao = Solution()
 n_nos = 5
 arestas = [[0, 1, 1], [1, 2, 1], [2, 3, 2], [0, 3, 2], [0, 4, 3], [3, 4, 3], [1, 4, 6]]
 
-arestas_criticas = solucao.encontrarArestasCriticas(n_nos, arestas)
-print( arestas_criticas)
+resultado = solucao.encontrarArestasCriticasEPseudoCriticas(n_nos, arestas)
+print("criticas:", resultado[0])
+print("pseudo:", resultado[1])
