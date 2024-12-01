ec3997f96f43d57581f1637752fb360513b62d13
Pedro <pedrohenben2014@gmail.com>
2024-12-01 15:28:21 -0300

    Add matriz de custo minimo

--- 1368.Minimun Cost to Make at Least One Valid Path in a Grid/1368.Minimun_Cost_to_Make_at_Least_One_Valid_Path_in_a_Grid.py
+++ 1368.Minimun Cost to Make at Least One Valid Path in a Grid/1368.Minimun_Cost_to_Make_at_Least_One_Valid_Path_in_a_Grid.py
@@ -2,30 +2,50 @@ import heapq
 
 class Solution:
     def minCost(self, grid):
+        m, n = len(grid), len(grid[0])
 
-        heap = [(0, 0, 0)]  # (custo, x, y) 
+        # Direções de movimentação
+        direções = [(0, 1), (0, -1), (1, 0), (-1, 0)] 
+
+        heap = [(0, 0, 0)]  # (custo, x, y)
+
+        custo_minimo = [[float('inf')] * n for _ in range(m)]
+        custo_minimo[0][0] = 0  
+
         passos = []  
+
+        def print_custo_minimo():  
+            print("Matriz custo minimo:")
+            for linha in custo_minimo:
+                print(linha)
+            print()
 
         while heap:
             custo, x, y = heapq.heappop(heap)  
-            passos.append(f"Desenfileirou: custo={custo}, x={x}, y={y}")
-            
-           
-            if x + 1 < len(grid):  
-                heapq.heappush(heap, (custo + 1, x + 1, y))
-                passos.append(f"Enfileirou: custo={custo + 1}, x={x + 1}, y={y}")
-            
-            if y + 1 < len(grid[0]):  
-                heapq.heappush(heap, (custo + 1, x, y + 1))
-                passos.append(f"Enfileirou: custo={custo + 1}, x={x}, y={y + 1}")
-            
-            if not heap:  
-                passos.append("Heap vazia.")
+            passos.append(f"Desenfileira: custo={custo}, x={x}, y={y}")
+
+            if custo > custo_minimo[x][y]:
+                passos.append(f"Ignorado: custo={custo} > custo_minimo={custo_minimo[x][y]}")
+                continue
+
+            for i, (dx, dy) in enumerate(direções):
+                nx, ny = x + dx, y + dy
+
+                novo_custo = custo + (1 if grid[x][y] != i + 1 else 0)
+
+                if novo_custo < custo_minimo[nx][ny]:  
+                    custo_minimo[nx][ny] = novo_custo
+                    heapq.heappush(heap, (novo_custo, nx, ny))
+                    passos.append(f"enfileira: custo={novo_custo}, x={nx}, y={ny}")
+                    print_custo_minimo()  
+
         
-        for passo in passos: 
+        for passo in passos:
             print(passo)
+
+        return custo_minimo
 
 if __name__ == "__main__":
     solution = Solution()
-    grid = [[1, 1, 1], [1, 1, 1], [1, 1, 1]] 
+    grid = [[1, 1, 3], [3, 2, 2], [1, 1, 4]]
     solution.minCost(grid)
